2ae873d9a44d9ae15b3a488878dd7e021396ad7b
Joshua Mabawonku <drakejoshua086@gmail.com>
2025-09-26 19:04:19 +0100

    update database string for mongoose

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb://localhost/taskify')
+        await mongoose.connect('mongodb+srv://joshuanathan086_db_user:OTiOswkhIObyfAfH@cluster0.wnbvsti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         console.log("DB connected successfully")
     } catch( err ) {
         console.log("DB connection error: ", err.message )
