639a22a5853a0624b75fb85f03e06f58135951f1
Joshua Mabawonku <drakejoshua086@gmail.com>
2025-09-28 23:14:16 +0100

    fix: updated passport google strategy to use right callback url as denoted in .env

--- configs/passport.js
+++ configs/passport.js
@@ -62,7 +62,7 @@ function initPassportAuthStrategies( passport ) {
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/auth/google/callback'
+            callbackURL: process.env.GOOGLE_CALLBACK_URL
         },
         function( accessToken, refreshToken, profile, done ) {
             return done( null, profile )
